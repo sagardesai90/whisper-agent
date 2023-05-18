020ae229800e261d8716f236ea30e902ecbb930f
sagardesai90 <saagardesai@gmail.com>
2023-05-17 22:43:57 -0700

    minor change

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head> */}
 
+      <div>
+        HackAIthon
+      </div>
+
       {/* <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
